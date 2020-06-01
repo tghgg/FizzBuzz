4921209fac948bb3e7104e0e653a56a05f065ea7
Falling Snowdin <falling_snowdin@protonmail.com>
2020-06-02 02:57:44 +0700

    Add some more description

--- hmm.js
+++ hmm.js
@@ -1,4 +1,5 @@
 /* This took all my brain juices, and is probably just a show-off contest in the end */
+/* This would not have been possible without the first few chapters of Eloquent Javascript. Go give it a read and add some rigor to your knowledge. Thanks my man the author. */
 
 for (let i = 1; i <= 100; i++) {
 	
